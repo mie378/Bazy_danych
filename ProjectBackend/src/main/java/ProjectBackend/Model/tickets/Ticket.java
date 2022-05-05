a721f8bcecc442ede298cc912178388a165642e5
mie378 <73713814+mie378@users.noreply.github.com>
2022-05-06 01:45:11 +0200

    Ticked model added

--- ProjectBackend/src/main/java/ProjectBackend/Model/tickets/Ticket.java
+++ ProjectBackend/src/main/java/ProjectBackend/Model/tickets/Ticket.java
@@ -1,4 +1,100 @@
 package ProjectBackend.Model.tickets;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Document("tickets")
 public class Ticket {
+    private int trainRouteID;
+    private String startingStation;
+    private String endingStation;
+    private BigDecimal price;
+    private BigDecimal discount;
+    private Date ticketDate;
+    private String travelerName;
+    private String travelerSurname;
+    private String travelerEmail;
+    private int userId;
+
+    public int getTrainRouteID() {
+        return trainRouteID;
+    }
+
+    public void setTrainRouteID(int trainRouteID) {
+        this.trainRouteID = trainRouteID;
+    }
+
+    public String getStartingStation() {
+        return startingStation;
+    }
+
+    public void setStartingStation(String startingStation) {
+        this.startingStation = startingStation;
+    }
+
+    public String getEndingStation() {
+        return endingStation;
+    }
+
+    public void setEndingStation(String endingStation) {
+        this.endingStation = endingStation;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public Date getTicketDate() {
+        return ticketDate;
+    }
+
+    public void setTicketDate(Date ticketDate) {
+        this.ticketDate = ticketDate;
+    }
+
+    public String getTravelerName() {
+        return travelerName;
+    }
+
+    public void setTravelerName(String travelerName) {
+        this.travelerName = travelerName;
+    }
+
+    public String getTravelerSurname() {
+        return travelerSurname;
+    }
+
+    public void setTravelerSurname(String travelerSurname) {
+        this.travelerSurname = travelerSurname;
+    }
+
+    public String getTravelerEmail() {
+        return travelerEmail;
+    }
+
+    public void setTravelerEmail(String travelerEmail) {
+        this.travelerEmail = travelerEmail;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
