48e83e12f375777d79b546608d0bb5ba962d65a6
mie378 <73713814+mie378@users.noreply.github.com>
2022-05-06 02:41:35 +0200

    Working communication with MongoDB

--- ProjectBackend/src/main/java/ProjectBackend/webservice/TicketController.java
+++ ProjectBackend/src/main/java/ProjectBackend/webservice/TicketController.java
@@ -1,4 +1,27 @@
 package ProjectBackend.webservice;
 
+import ProjectBackend.Model.tickets.Ticket;
+import ProjectBackend.data.tickets.TicketDBController;
+import ProjectBackend.data.tickets.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Service
+@RestController
+@RequestMapping("/tickets")
 public class TicketController {
+    @Autowired
+    TicketDBController ticketDBController;
+    public TicketController(TicketDBController ticketDBController){this.ticketDBController=ticketDBController;}
+    @RequestMapping(path="/buy",method= RequestMethod.POST)
+    public String buyTicket(){
+        return "OK";
+    }
+
 }
